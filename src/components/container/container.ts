99958eade6f28b4b5af605af4f99c5d05bc4b737
Sean Wong <seanwong24@live.com>
2023-07-25 07:03:20 -0700

    implement the logic

--- src/components/container/container.ts
+++ src/components/container/container.ts
@@ -1,14 +1,50 @@
-import { LitElement, css, html } from "lit";
+import { LitElement, html, unsafeCSS } from "lit";
 import { customElement } from "lit/decorators.js";
 import elementCSS from "./container.css?inline";
 
 @customElement(`ms-container`)
 export class MagicScrollContainer extends LitElement {
+  static styles = [unsafeCSS(elementCSS)];
+
+  firstUpdated() {
+    this.#updateCanvasSize();
+  }
+
+  constructor() {
+    super();
+    this.#setCanvasSizeWhenResized();
+    this.addEventListener(
+      "scroll",
+      ({ currentTarget }) => currentTarget === this && this.#updateScrollRatio()
+    );
+  }
+
   render() {
     return html` <slot></slot> `;
   }
 
-  static styles = [css([elementCSS] as any)];
+  #setCanvasSizeWhenResized() {
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        if (entry.target === this) {
+          this.#updateCanvasSize();
+        }
+      }
+    });
+    resizeObserver.observe(this);
+  }
+
+  #updateCanvasSize() {
+    const height = this.clientHeight;
+    this.style.setProperty("--canvas-height", `${height}px`);
+    const width = this.clientWidth;
+    this.style.setProperty("--canvas-width", `${width}px`);
+  }
+
+  #updateScrollRatio() {
+    const scrollRatio = this.scrollTop / this.clientHeight;
+    this.style.setProperty("--scroll-ratio", scrollRatio.toString());
+  }
 }
 
 declare global {
--- src/components/item/item.ts
+++ src/components/item/item.ts
@@ -1,14 +1,52 @@
-import { LitElement, css, html } from "lit";
-import { customElement } from "lit/decorators.js";
+import { LitElement, html, unsafeCSS } from "lit";
+import { customElement, property } from "lit/decorators.js";
 import elementCSS from "./item.css?inline";
+
+type CSSStringOrFunction = string | ((scroll: number) => string);
 
 @customElement(`ms-item`)
 export class MagicScrollItem extends LitElement {
+  static styles = [unsafeCSS(elementCSS)];
+
+  #pageIndex = 0;
+  get pageIndex() {
+    return this.#pageIndex;
+  }
+  @property({ type: Number, attribute: "page-index" }) set pageIndex(
+    value: number
+  ) {
+    this.#pageIndex = value;
+    this.style.setProperty("--page-index", value.toString());
+  }
+
+  #transform: CSSStringOrFunction = "";
+  get transform() {
+    return this.#transform;
+  }
+  @property() set transform(value: CSSStringOrFunction) {
+    this.#transform = value;
+    this.#updateTransform();
+  }
+
+  get #actualTransform() {
+    const transform = this.transform;
+    switch (typeof transform) {
+      case "string":
+        return transform;
+      case "function":
+        return transform(+getComputedStyle(this).getPropertyValue("--scroll"));
+      default:
+        return "";
+    }
+  }
+
   render() {
     return html` <slot></slot> `;
   }
 
-  static styles = [css([elementCSS] as any)];
+  #updateTransform() {
+    this.style.transform = this.#actualTransform;
+  }
 }
 
 declare global {
